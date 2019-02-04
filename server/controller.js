fdc6402b2038a3defadabfd5d1395a9aa8d3548a
Your Name <you@example.com>
2019-02-04 14:42:56 -0700

    removed a console log

--- server/controller.js
+++ server/controller.js
@@ -1,22 +1,21 @@
-module.exports={
-    getAll:(req, res) => {
+module.exports = {
+    getAll: (req, res) => {
         req.app.get('db').get_all().then((properties) => {
             res.status(200).send(properties);
         })
-        .catch(err=>{
-            console.log(err)
-        })   
+            .catch(err => {
+                console.log(err)
+            })
     },
-    createProperty:(req, res) => {
-        const {propertyname,address,city,state,zip,imageurl,monthlymortgageamount,desiredmonthlyrent} = req.body;
-        req.app.get('db').create_property([propertyname,address,city,state,zip,imageurl,monthlymortgageamount,desiredmonthlyrent])
+    createProperty: (req, res) => {
+        const { propertyname, address, city, state, zip, imageurl, monthlymortgageamount, desiredmonthlyrent } = req.body;
+        req.app.get('db').create_property([propertyname, address, city, state, zip, imageurl, monthlymortgageamount, desiredmonthlyrent])
             .then(() => {
                 res.sendStatus(200)
             })
     },
-    deleteProperty:(req, res) => {
+    deleteProperty: (req, res) => {
         const { id } = req.params
-        console.log(id)
         req.app.get('db').delete_property(id).then(() => {
             res.sendStatus(200)
         })
